e79ccb9644ec4ecba54b30f7ee63fddddf54faa7
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-17 00:18:19 -0400

    Updated feature where now it accepts two numbers and picks a random number between them

--- js/random.js
+++ js/random.js
@@ -1,21 +1,12 @@
-// Collect input from a user
-const number = prompt("Please enter a number: ");
 
-// Convert the input to a number
-const convert = +number;
+const numberone = prompt("Please enter your low number: ");
+const numbertwo = prompt("Please enter your high number: ")
+const convertone = +numberone;
+const converttwo = +numbertwo; 
 
-// Use Math.random() and the user's number to generate a random number
-const random = Math.floor(Math.random()*convert + 1);
-
-// Create a message displaying the random number
-
-
-const final = `<p> Your random number is ${random} </p>`;
-
-document.querySelector('main').innerHTML = final; 
-
-if (convert){
-	const final = `<p> Your random number is ${random} </p>`;
+if (convertone && converttwo){
+	const random = Math.floor(Math.random()*(converttwo - convertone + 1)) + convertone;
+	const final = `<p> Your random number between ${convertone} and ${converttwo} is: ${random} </p>`;
 	document.querySelector('main').innerHTML = final; 
 } else {
 	const final = `<p> You did not enter a number value. Please try again. </p>`;
