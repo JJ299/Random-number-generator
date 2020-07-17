2eb2c5921ce31b4d522e18984fc0e2372dfe985b
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-17 00:00:12 -0400

    Added more features where the site will give you an error if you type in a string instead of a number

--- js/random.js
+++ js/random.js
@@ -1,11 +1,23 @@
 // Collect input from a user
-
+const number = prompt("Please enter a number: ");
 
 // Convert the input to a number
-
+const convert = +number;
 
 // Use Math.random() and the user's number to generate a random number
-
+const random = Math.floor(Math.random()*convert + 1);
 
 // Create a message displaying the random number
 
+
+const final = `<p> Your random number is ${random} </p>`;
+
+document.querySelector('main').innerHTML = final; 
+
+if (convert){
+	const final = `<p> Your random number is ${random} </p>`;
+	document.querySelector('main').innerHTML = final; 
+} else {
+	const final = `<p> You did not enter a number value. Please try again. </p>`;
+	document.querySelector('main').innerHTML = final; 
+}
